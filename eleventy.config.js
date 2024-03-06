7dfee404b3b6c6853813f85417e34d4e1b857ac6
richardguenther <richardguenther@fastmail.com>
2024-03-06 15:45:19 +0100

    first commit

---
+++ eleventy.config.js
@@ -0,0 +1,23 @@
+export default async function (eleventyConfig) {
+	// Copy `11ty_css/` to `_site/css`
+	eleventyConfig.addPassthroughCopy({ "11ty_css": "/css" });
+	// Copy `11ty_js/` to `_site/js`
+	eleventyConfig.addPassthroughCopy({ "11ty_js": "/js" });
+
+	eleventyConfig.setServerOptions({
+		domDiff: true,
+		port: 4321,
+		// e.g. `watch: ["_site/**/*.css"]`
+		watch: [],
+	});
+
+	return {
+		dir: {
+			input: "11ty_content",
+			output: "_site",
+			// ⚠️ These values are both relative to your input directory.
+			includes: "../11ty_includes",
+			layouts: "../11ty_layouts",
+		},
+	};
+}
